c2e346b786466cf0f8d8a21c60cc8d5b3d9f2e1c
maozkosover <maozkosover@gmail.com>
2021-06-16 15:02:25 +0300

    viewmodel

--- app/src/main/java/com/example/remotejoystick/views/MainActivity.kt
+++ app/src/main/java/com/example/remotejoystick/views/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.remotejoystick.views
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.remotejoystick.R
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button7).setOnClickListener{
+            doneClicked(it)
+        }
+    }
+
+    fun doneClicked(view: View) {
+
     }
 }
